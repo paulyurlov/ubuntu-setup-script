40df8d85243af666839991feb09ef0fcc60d3e04
Paul Yurlov <paulyurlov@ya.ru>
2022-12-30 19:08:01 +0300

    Add script and dconf file

---
+++ my_setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+user=$(whoami)
+echo "Hi, $user! Please enter your password on the next step =)"
+
+# Update and upgrade
+sudo apt update
+sudo apt upgrade
+
+# Remove snap-store and install gnome-software with flatpak
+sudo snap remove --purge snap-store
+sudo apt install flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Setup fish shell as default
+sudo apt install fish
+chsh -s /usr/bin/fish
+
+# Prepare for theme installation
+sudo apt install curl gnome-shell-extensions git unzip gnome-tweaks
+
+# Clone Orchis theme for user shell
+git clone https://github.com/vinceliuice/Orchis-theme.git
+
+# Clone WhiteSur theme for main theme
+git clone https://github.com/EliverLara/WhiteSur-gtk-theme.git
+
+# Clone Vimix cursor theme
+git clone https://github.com/vinceliuice/Vimix-cursors.git
+
+# Clone Reversal theme for icons
+git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
+
+# Install Orchis theme
+./Orchis-theme/install.sh -t orange --tweaks compact
+
+# Install WhiteSur theme
+./WhiteSur-gtk-theme/install.sh -t orange
+
+# Connect WhiteSur theme to flatpak
+./WhiteSur-gtk-theme/tweak.sh -F
+
+# Install Reversal theme
+./Reversal-icon-theme/install.sh -orange
+
+# Install Vimix theme
+cd Vimix-cursors
+sudo ./install.sh
+cd ~
+
+# Enable WhiteSur as main theme
+gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-Dark-solid-orange"
+gsettings set org.gnome.desktop.wm.preferences theme "WhiteSur-Dark-solid-orange"
+
+# Enable Reversal as icon theme
+gsettings set org.gnome.desktop.interface icon-theme "Reversal-orange-dark"
+
+# Load dconf settings
+dconf load / <my-dconf-settings.ini
+
+# Clear installation files
+rm -rf Orchis-theme
+rm -rf WhiteSur-gtk-theme
+rm -rf Reversal-icon-theme
+rm -rf Vimix-cursors
+rm -rf my-dconf-settings.ini
+
+# Reboot part
+read -n 1 -p "Do uou want to reboot to apply settings (y/n)? " answer
+printf "\n\n"
+if [[ $answer == "y" ]]; then
+  echo "Rebooting in 3 seconds"
+  sudo reboot -f
+else
+  echo "No reboot"
+fi
