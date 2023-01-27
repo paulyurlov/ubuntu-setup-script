78516b7346d94cb9e2449de5b7e6d17fab61911c
Paul Yurlov <palyurlov@gmail.com>
2023-01-27 10:57:03 +0300

    Minor update

--- my_setup.sh
+++ my_setup.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 user=$(whoami)
-echo "Hi, $user! Please enter your password: "
-read pass
-export pass
-echo $pass
+echo "Hi, $user!"
 
 # Update and upgrade
-echo "$pass" | sudo --stdin apt update
+sudo apt update
 sudo apt upgrade
 
 # Remove snap-store and install gnome-software with flatpak
+
+echo "Remove snap-store and install gnome-software with flatpak"
 sudo snap remove --purge snap-store
 sudo apt install flatpak gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Setup fish shell as default
+
+echo "Setup fish shell as default"
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
@@ -26,6 +27,7 @@ sudo apt install fonts-roboto
 
 # Install oh-my-posh and theme
 
+echo "Install oh-my-posh and theme"
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
@@ -56,18 +58,28 @@ git clone https://github.com/vinceliuice/Vimix-cursors.git
 git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
 
 # Install Orchis theme
+
+echo "Install Orchis theme"
 ./Orchis-theme/install.sh -t orange --tweaks compact
 
 # Install WhiteSur theme
+
+echo "Install WhiteSur theme"
 ./WhiteSur-gtk-theme/install.sh -t orange
 
 # Connect WhiteSur theme to flatpak
+
+echo "Connect WhiteSur theme to flatpak"
 ./WhiteSur-gtk-theme/tweak.sh -F
 
 # Install Reversal theme
+
+echo "Install Reversal theme"
 ./Reversal-icon-theme/install.sh -orange
 
 # Install Vimix theme
+
+echo "Install Vimix theme"
 cd Vimix-cursors
 sudo ./install.sh
 cd ~
@@ -83,6 +95,7 @@ gsettings set org.gnome.desktop.interface icon-theme "Reversal-orange-dark"
 dconf load / <my-dconf-settings.ini
 
 # Install Conky
+echo "Setup conky"
 sudo apt install conky-all curl jq imagemagick python3-pip
 pip install geocoder yaweather geopy
 
@@ -102,21 +115,63 @@ rm -rf Reversal-icon-theme
 rm -rf Vimix-cursors
 rm -rf my-dconf-settings.ini
 
+printf "\n\n"
 # Install Anaconda
-curl "https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh"
 read -n 1 -p "Do uou want to install Anaconda (y/n)? " answer
 printf "\n\n"
 if [[ $answer == "y" ]]; then
   echo "Starting anaconda installation"
   printf "\n\n"
   wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+  sudo chmod +x Anaconda3-2022.10-Linux-x86_64.sh
+  ./Anaconda3-2022.10-Linux-x86_64.sh
+  conda init fish
+  conda config --set auto_activate_base false
 else
   echo "Skiping anaconda installation"
 fi
 
 # Install VS-code
 sudo snap install code --classic
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/RobotoMono.zip
+
+# Install Telegram
+flatpak install flathub org.telegram.desktop
+
+# Install Discord
+flatpak install flathub com.discordapp.Discord
+
+# Install Zoom
+flatpak install flathub us.zoom.Zoom
+
+# Install Steam
+sudo apt install steam
+
+# Install Termius
+sudo snap install termius-app
+
+# Install MarkText
+flatpak install flathub com.github.marktext.marktext
+
+# Install Synaptic
+sudo apt install synaptic
+
+# Install WPS office
+flatpak install flathub com.wps.Office
+
+# Install GitHub CLI
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+  sudo apt update &&
+  sudo apt install gh -y
+
+# Install Yandex Browser
+# sudo add-apt-repository "deb https://repo.yandex.ru/yandex-browser/deb stable main"
+# curl https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG --output YANDEX-BROWSER-KEY.GPG
+# sudo apt-key add YANDEX-BROWSER-KEY.GPG
+
+sudo apt autoremove
 
 # Reboot part
 read -n 1 -p "Do uou want to reboot to apply settings (y/n)? " answer
